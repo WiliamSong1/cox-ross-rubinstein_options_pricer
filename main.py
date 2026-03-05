1d6f2b5ad24bf87b39b76d7ba4f84a6fa8861bee
William Song <107579359+WiliamSong1@users.noreply.github.com>
2026-03-04 23:27:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,6 @@ def pricingRequest(stockPrice, strikePrice, timeToMaturity, marketRate, marketVo
     sUp = stockPrice * uVal
     delta = (vUp - vDown)/(sUp - sDown)
     price = values[0]
-    print(price)
-    print(delta)
-    print(numOfSteps)
-    print(uVal, dVal, vUp, vDown, sUp, sDown)
 pricingRequest(100, 100, 1, 0.05, 0.20, 200)
 
 
